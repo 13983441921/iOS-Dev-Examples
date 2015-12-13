54fef30904c8047ec00764a6db4769078202b014
zhanghanhui <zhanghanhui@oneniceapp.com>
2015-12-13 10:55:54 +0800

    UIControlExample

---
+++ UIKit/UIView/2. UIControlExample/UIControlExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIControlExample
+//
+//  Created by nice on 15/12/1.
+//  Copyright © 2015年 iosdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
